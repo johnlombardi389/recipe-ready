26bf8ac902c5f13b17651bc1cfa59a56b185c860
johnlombardi389 <lombardij38@gmail.com>
2023-09-20 15:53:31 -0400

    Created nav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { Routes, Route } from "react-router-dom";
 // Style
 import GlobalStyles from "./components/GlobalStyle";
+// Components
+import Nav from "./components/Nav";
+// Pages
 
 function App() {
   return (
     <>
       <GlobalStyles />
+      <Nav />
       <div>
         <h1>Hello</h1>
       </div>
---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom";
+// Style
+import styled from "styled-components";
+
+const Nav = () => {
+  return (
+    <nav>
+      <h1>This is the nav</h1>
+    </nav>
+  );
+};
+
+export default Nav;
