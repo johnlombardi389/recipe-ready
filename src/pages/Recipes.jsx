cc9487565b1981c924482843e374a0a5e3627673
johnlombardi389 <lombardij38@gmail.com>
2023-10-06 20:07:42 -0400

    Added spoonacular api to search recipes by ingredient

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,7 +1,56 @@
+import { useState } from "react";
+import axios from "axios";
+// Style
+import styled from "styled-components";
+
 const Recipes = () => {
+  const [ingredients, setIngredients] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const apiKey = import.meta.env.VITE_REACT_APP_API_KEY;
+
+  const handleSearch = async () => {
+    setLoading(true);
+
+    try {
+      const response = await axios.get(
+        `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=${ingredients}`
+      );
+
+      setRecipes(response.data);
+    } catch (error) {
+      console.error("Error fetching recipes:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <>
-      <h1>This is the recipes page</h1>
+      <h1>Recipes</h1>
+
+      <div>
+        <h1>Recipe Search</h1>
+        <div>
+          <label htmlFor="ingredients">
+            Enter Ingredients (comma-separated):
+          </label>
+          <input
+            type="text"
+            id="ingredients"
+            value={ingredients}
+            onChange={(e) => setIngredients(e.target.value)}
+          />
+          <button onClick={handleSearch}>Search</button>
+        </div>
+        {loading && <p>Loading...</p>}
+        <ul>
+          {recipes.map((recipe) => (
+            <li key={recipe.id}>{recipe.title}</li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
