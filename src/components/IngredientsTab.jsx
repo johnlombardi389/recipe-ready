51b58665b462a5afc221ffe3620e487c0067837b
johnlombardi389 <lombardij38@gmail.com>
2023-10-10 15:27:27 -0400

    Styled ingredient lists within recipe component

--- src/components/IngredientsTab.jsx
+++ src/components/IngredientsTab.jsx
@@ -1,20 +1,21 @@
-import React from "react";
+// Style
+import styled from "styled-components";
 
 const IngredientsTab = ({ recipe }) => {
   return (
     <>
-      <h3>All Ingredients</h3>
-      <ul>
+      <StyledTitle>Recipe Ingredients</StyledTitle>
+      <StyledList>
         {recipe.details.extendedIngredients.map((ingredient) => (
           <li key={ingredient.id}>
             {ingredient.name}: {ingredient.measures.metric.amount}
             {ingredient.measures.metric.unitLong}
           </li>
         ))}
-      </ul>
+      </StyledList>
 
-      <h3>Missing Ingredients:</h3>
-      <ul>
+      <StyledTitle>You Need to Get</StyledTitle>
+      <StyledList>
         {recipe.missedIngredients ? (
           recipe.missedIngredients.map((ingredient) => (
             <li key={ingredient.id}>{ingredient.name}</li>
@@ -22,9 +23,27 @@ const IngredientsTab = ({ recipe }) => {
         ) : (
           <li>No missing Ingredients</li>
         )}
-      </ul>
+      </StyledList>
     </>
   );
 };
 
 export default IngredientsTab;
+
+const StyledTitle = styled.h3`
+  font-size: 1.2rem;
+  margin-bottom: 1rem;
+`;
+
+const StyledList = styled.ul`
+  list-style-type: disc;
+  padding-left: 1.5rem;
+  margin-bottom: 2rem;
+
+  li {
+    margin-bottom: 0.25rem;
+    font-size: 1rem;
+    color: #333;
+    text-transform: capitalize;
+  }
+`;
