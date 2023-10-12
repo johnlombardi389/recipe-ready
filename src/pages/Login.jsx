04234468117bf5284954ddfbe499075619c343ef
johnlombardi389 <lombardij38@gmail.com>
2023-10-12 14:56:02 -0400

    Styled login page form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -22,40 +22,6 @@ const Login = () => {
     });
   };
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-
-  //   axios
-  //     .post("http://localhost:8000/api/login/", credentials) // Updated endpoint
-  //     .then((response) => {
-  //       // save access token to local storage
-  //       const { access, refresh } = response.data;
-  //       localStorage.setItem("access_token", access);
-  //       localStorage.setItem("refresh_token", refresh);
-
-  //       axios
-  //         .get("http://localhost:8000/api/user/", {
-  //           headers: {
-  //             Authorization: `Bearer ${access}`, // Include the access token in the request headers
-  //           },
-  //         })
-  //         .then((userResponse) => {
-  //           const { id } = userResponse.data;
-  //           localStorage.setItem("user_id", id); // Save the user's ID in local storage
-
-  //           login();
-  //           // redirect
-  //           navigate("/");
-  //         })
-  //         .catch((error) => {
-  //           console.error("Error getting user information:", error);
-  //         });
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error logging in:", error);
-  //     });
-  // };
-
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -101,9 +67,9 @@ const Login = () => {
   };
 
   return (
-    <>
+    <LoginContainer>
       {!isLoggedIn ? (
-        <form onSubmit={handleSubmit}>
+        <StyledForm onSubmit={handleSubmit}>
           <div>
             <label htmlFor="username">Username</label>
             <input
@@ -126,13 +92,65 @@ const Login = () => {
             />
           </div>
           <button type="submit">Login</button>
-          <Link to="/register">Don't have an account? Sign up here</Link>
-        </form>
+          <Link className="register" to="/register">
+            Don't have an account? Sign up here
+          </Link>
+        </StyledForm>
       ) : (
         <button onClick={handleLogout}>Logout</button>
       )}
-    </>
+    </LoginContainer>
   );
 };
 
 export default Login;
+
+const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 2rem auto;
+  max-width: 300px;
+  padding: 1.5rem;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background-color: #fff;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+`;
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+
+  label {
+    font-size: 1rem;
+  }
+
+  input {
+    width: 100%;
+    padding: 0.5rem;
+    font-size: 1rem;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    margin: 0.5rem 0 1.5rem 0;
+  }
+
+  button {
+    background-color: #0073e6;
+    color: white;
+    padding: 0.5rem;
+    font-size: 1rem;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    margin: 1rem 0;
+  }
+
+  .register {
+    text-align: center;
+    color: grey;
+    &:hover {
+      color: black;
+    }
+  }
+`;
