39719e6f8b4c0f94e9528c2cf95d1c18f94c27a1
johnlombardi389 <lombardij38@gmail.com>
2023-10-31 16:14:48 -0400

    Styled no ingredients found component for smaller screens

--- src/components/NoIngredients.jsx
+++ src/components/NoIngredients.jsx
@@ -31,6 +31,9 @@ const StyledContainer = styled.div`
   margin: 2rem auto;
   border-radius: 2rem;
   padding: 5rem;
+  @media (max-width: 1000px) {
+    padding: 1rem;
+  }
 `;
 
 const StyledMessage = styled.div`
@@ -53,5 +56,16 @@ const StyledMessage = styled.div`
     font-size: 0.9rem;
     font-weight: 400;
     color: rgba(0, 0, 0, 0.5);
+  }
+  @media (max-width: 1000px) {
+    h3 {
+      font-size: 1.5rem;
+    }
+    .first-text {
+      font-size: 0.95rem;
+    }
+    .second-text {
+      font-size: 0.85rem;
+    }
   }
 `;
